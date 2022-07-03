809a01c7a8679a984fce2f9a6bc1bc660279c0bf
NANO <File changed>
2022-07-03 21:41:44 +0600

    1 submission

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+       int c = 5;
+
+        System.out.println(toBeDivided/c);
     }
 }
--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println(first+""+second+""+third);
     }
 }

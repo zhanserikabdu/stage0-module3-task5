7f33e14aec3cb75c2b5434b2e831fda01249060e
NANO <File changed>
2022-07-14 23:06:36 +0600

    4 submission

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task5;
 public class NumberDivider {
     void divide(int toBeDivided) {
         int d = 5;
-       int b = toBeDivided % d;
+       int b = toBeDivided / d;
 
         System.out.println(b);
     }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task5;
 public class TimeConvertor {
     public void convert(float minutes) {
 
-        int n = Math.round(minutes);
-        System.out.println(n);
+        int seconds = Math.round(minutes);
+        System.out.println(seconds);
     }
 }

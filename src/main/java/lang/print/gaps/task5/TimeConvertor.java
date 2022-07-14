202a6f8f069392f9dad59153dc0a6c1c87e5586c
NANO <File changed>
2022-07-14 23:02:47 +0600

    4 submission

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task5;
 public class TimeConvertor {
     public void convert(float minutes) {
 
-        int seconds = Math.round(minutes);
-        System.out.println(seconds);
+        int n = Math.round(minutes);
+        System.out.println(n);
     }
 }
